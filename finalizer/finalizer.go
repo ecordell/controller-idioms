d2eb08c72157946922a008761bb92ad8c8c6a519
ivanauth <ivan@authzed.com>
2026-05-26 15:15:18 -0400

    fix: use stdlib slices to satisfy govet inline check
    golangci-lint v2.12.2's govet flags slices calls from golang.org/x/exp/slices
    as un-inlineable because type-parameter inference for inlining isn't
    supported. Switching to the stdlib slices package (available since Go 1.21)
    resolves the warnings.

--- finalizer/finalizer.go
+++ finalizer/finalizer.go
@@ -13,8 +13,8 @@ package finalizer
 
 import (
 	"context"
+	"slices"
 
-	"golang.org/x/exp/slices"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
